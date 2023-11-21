d8bad2ab21b0e8e7d73ddf5992599a491a4b6b4b
audkwann <99209223+audkwann@users.noreply.github.com>
2023-11-21 11:08:34 -0800

    Update util.py

--- util.py
+++ util.py
@@ -1,17 +1,19 @@
 from typing import List, Tuple, Dict, Any, Union, Optional, Iterable
+import random
 
 StateT = [str, str, Tuple[int, int]] #isStart, isEnd, position
 Actions = Any
 
 class BCubed:
     # initialize important variables
-    def __init__(self, x = 4, y = 4, endState = (), startState = (0, 0)):
+    def __init__(self, x = 4, y = 4, endState = (), startState = (0, 0), explorationProb: float = 0.2):
         self.grid = (x, y)
         self.visitedPositions = []
         self.actions = {}
         self.position = startState
         self.endState = endState
         self.void = []
+        self.explorationProb = explorationProb
         
 
     
@@ -27,7 +29,7 @@ class BCubed:
     input: self
     returns: dict{action: transition probability}
     """
-    def getActions(self) -> Dict[Actions]:
+    def getValidActions(self) -> Dict[Actions]:
         actions = {} # direction: probability
         possible = []
         x = self.position[0]
@@ -52,6 +54,36 @@ class BCubed:
             actions[action] = (1 / total)
         
         return actions
+
+    """
+    This function takes in the variables of the current game
+    state and returns the next action to take. This function uses
+    the epsilon-greedy algorithm, choosing a random action with
+    probability = explorationProb. Otherwise, it will pick an 
+    action from the list of valid actions, according to the 
+    transition probability of each action.
+    
+    NOTE TO MAHATHI: in the pset, epsilon-greedy outputted the action given by
+    the optimal policy with prob: 1 - explorationProb. So I'm not sure if this function 
+    is right.
+
+    input: self
+    returns: action
+    """
+    def getAction(self) -> tuple:
+        validActions = self.getValidActions()
+
+        explorationProb = self.explorationProb
+        options = ["exploit", "explore"]
+        chosen_option = random.choices(options, weights=(1-explorationProb, explorationProb), k=1)
+
+        if chosen_option[0] == "explore":
+            action = random.choices(list(validActions.keys()))
+            return action[0]
+        elif chosen_option[0] == "exploit":
+            """NOTE TO MAHATHI: regarding earlier note, should we change this to match the optimal policy??? 
+            instead of using validActions?"""
+            return random.choices(list(validActions.keys()), weights=list(validActions.values()), k=1)[0]
     
     def policy():
         return action
